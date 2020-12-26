8f01496776ed90d114800290a16183ed0eb3dad1
jojo <774948439@qq.com>
2020-12-26 23:53:13 +0800

    构造函数shu xing   构造函数属性属性使用了解

--- node_global/a-2.js
+++ node_global/a-2.js
@@ -23,3 +23,21 @@ console.log(a1Module);
  * module.exports = name; 
  * jojo
  */
+function A() {
+    this.a = 1
+}
+A.prototype.b = function() {
+    console.log('this-b', this) // A { a: 1 }
+    return 3 + this.a // 4可以拿到this
+}
+A.c = function() {
+    console.log('this-c', this) // [Function: A] { c: [Function (anonymous)] }
+    return 5 + this.a // NaN 因为拿不到this.a
+}
+var AA = new A()
+console.log(A) // [Function: A]
+console.log(AA) // { c: [Function (anonymous)] }
+console.log('调用b', AA.b()) // 仅实例可调用
+// console.log('调用b', A.b()) // 构造函数自己调用不了
+// console.log('调用c', AA.c()) // 实例调用不了
+console.log('调用c', A.c()) // 仅构造函数自己调用
